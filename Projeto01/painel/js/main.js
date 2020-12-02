84658e23e38c4deae97706318e8477b3548cfba7
Marcos Vinicius Gasparoto Bauab <marcosbauab@gmail.com>
2020-12-01 21:35:08 -0300

    Projeto 01
    -Painel atualizado

--- Projeto01/painel/js/main.js
+++ Projeto01/painel/js/main.js
@@ -1,21 +1,23 @@
 var windowSize = window.innerWidth
 
-
+var size = 'big'
 
 const sidebar = document.querySelector("aside")
 const content = document.querySelector("section.content")
 const header = document.querySelector("header")
 const hamburger = document.querySelector(".menu-btn")
 if(windowSize <= 768){
+    size = 'small'
     hamburger.classList.add("clicado")
 }
 hamburger.addEventListener("click", function(){
     hamburger.classList.toggle("clicado")
-    if(hamburger.className == "menu-btn"){
+    if(hamburger.className == "menu-btn" && size == 'big'){
         sidebar.animate([
             // keyframes
-            { width: '0', padding: '0' },
-            { width: '20%', padding: '10px' }
+            //{ width: '0', padding: '0' },
+  
+            { width: '20%', padding: '10px 0' }
           ], 
           { 
             // timing options
@@ -26,8 +28,8 @@ hamburger.addEventListener("click", function(){
           })
         header.animate([
             // keyframes
-            { width: '100%',
-              left : '0%' },
+            //{ width: '100%',
+            //  left : '0%' },
             { width: '80%',
               left: '20%' }
           ], 
@@ -40,8 +42,8 @@ hamburger.addEventListener("click", function(){
           })
         content.animate([
             // keyframes
-            { width: '100%',
-              left : '0%' },
+            //{ width: '100%',
+            //  left : '0%' },
             { width: '80%',
               left: '20%' }
           ], 
@@ -52,10 +54,10 @@ hamburger.addEventListener("click", function(){
             easing: 'ease',
             fill: 'forwards'
           })
-    } else {
+    } else if(size == 'big') {
         sidebar.animate([
             // keyframes
-            { width: '20%' },
+           // { width: '20%' },
             { width: '0',
               padding: '0' }
           ], 
@@ -68,8 +70,8 @@ hamburger.addEventListener("click", function(){
           })
         header.animate([
             // keyframes
-            { width: '80%',
-              left : '20%' },
+            //{ width: '80%',
+             // left : '20%' },
             { width: '100%',
               left: '0%' }
           ], 
@@ -82,8 +84,93 @@ hamburger.addEventListener("click", function(){
           })
         content.animate([
             // keyframes
-            { width: '80%',
-              left : '20%' },
+           // { width: '80%',
+            //  left : '20%' },
+            { width: '100%',
+              left: '0%' }
+          ], 
+          { 
+            // timing options
+            duration: 1000,
+            iterations: 1,
+            easing: 'ease',
+            fill: 'forwards'
+          })
+    }
+    if(hamburger.className == "menu-btn" && size == 'small'){
+      sidebar.animate([
+          // keyframes
+          //{ width: '0', padding: '0' },
+
+          { width: '50%', padding: '10px 0' }
+        ], 
+        { 
+          // timing options
+          duration: 1000,
+          iterations: 1,
+          easing: 'ease',
+          fill: 'forwards'
+        })
+      header.animate([
+          // keyframes
+          //{ width: '100%',
+          //  left : '0%' },
+          { width: '50%',
+            left: '50%' }
+        ], 
+        { 
+          // timing options
+          duration: 1000,
+          iterations: 1,
+          easing: 'ease',
+          fill: 'forwards'
+        })
+      content.animate([
+          // keyframes
+          //{ width: '100%',
+          //  left : '0%' },
+          { width: '50%',
+            left: '50%' }
+        ], 
+        { 
+          // timing options
+          duration: 1000,
+          iterations: 1,
+          easing: 'ease',
+          fill: 'forwards'
+        })
+    } else if(size == 'small') {
+        sidebar.animate([
+            // keyframes
+          // { width: '20%' },
+            { width: '0',
+              padding: '0' }
+          ], 
+          { 
+            // timing options
+            duration: 1000,
+            iterations: 1,
+            easing: 'ease',
+            fill: 'forwards'
+          })
+        header.animate([
+            // keyframes
+            //{ width: '80%',
+            // left : '20%' },
+            { width: '100%',
+              left: '0%' }
+          ], 
+          { 
+            // timing options
+            duration: 1000,
+            iterations: 1,
+            easing: 'ease',
+            fill: 'forwards'
+          })
+        content.animate([
+            // keyframes
+          // { width: '80%',
+            //  left : '20%' },
             { width: '100%',
               left: '0%' }
           ], 
--- Projeto01/painel/main.php
+++ Projeto01/painel/main.php
@@ -12,6 +12,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="<?php echo INCLUDE_PATH_PAINEL ?>css/painel.css">
+        <link rel="stylesheet" href="<?php echo INCLUDE_PATH_PAINEL ?>css/menu.css">
         <link rel="stylesheet" href="<?php echo INCLUDE_PATH?>styles/all.min.css">
         <title>Painel de controle</title>
     </head>
@@ -33,6 +34,21 @@
                         <p><?php echo pegaCargo($_SESSION['cargo']); ?></p>
                     </div>
                 </div>
+                <div class="itens-menu">
+                    <h2>Cadastro</h2>
+                    <a href="">Depoimentos</a>
+                    <a href="">Serviços</a>
+                    <a href="">Slides</a>
+                    <h2>Gestão</h2>
+                    <a href="">Listar Depoimentos</a>
+                    <a href="">Listar Serviços</a>
+                    <a href="">Listar Slides</a>
+                    <h2>Administração do Painel</h2>
+                    <a href="">Editar usuário</a>
+                    <a href="">Adicionar usuários</a>
+                    <h2>Configurações</h2>
+                    <a href="">Editar</a>
+                </div>
             </div>
         </aside>
         <header>
@@ -47,6 +63,22 @@
         </header>
         <section class="content">
             <div class="box-conteudo w100">
+                <h2><i class="fa fa-home"></i>Painel de controle</h2>
+                
+                <div class="container-metricas">
+                    <div class="box-metrica">
+                        <h2>Usuários online</h2>
+                        <p>10</p>
+                    </div>
+                    <div class="box-metrica">
+                        <h2>Visitas mensais</h2>
+                        <p>100</p>
+                    </div>
+                    <div class="box-metrica">
+                        <h2>Visitas Hoje</h2>
+                        <p>10</p>
+                    </div>
+                </div>
 
             </div>
             <div class="box-conteudo w50 left">
